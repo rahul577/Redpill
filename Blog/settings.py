ecc6534702c637d06872af13a3a747d99c313bc2
rahul577 <rahulbafila7@gmail.com>
2019-03-19 12:59:32 +0530

    added allowed host

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm(1nz13$2hh02qslwl+-g@+#7m+pcpdlhyhj4ev+c=w#2uxz7#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['redpillbrogram.herokuapp.com']
 
 
 # Application definition
