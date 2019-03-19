0d1de4c8658c37f95aa4ee1012bc5b969e596e1d
rahul577 <rahulbafila7@gmail.com>
2019-03-19 18:42:19 +0530

    dictofdict

--- api/views.py
+++ api/views.py
@@ -5,8 +5,31 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 import json
 
-
 threshold = 0
+def analyze_headline(headline):
+    polarity = TextBlob(headline).sentiment.polarity
+    if polarity >= threshold:
+        #positive
+        return "positive"
+    else:
+        #negative
+        return "negative"
+
+API_KEY  = "318d913237d64b00b18eefa946b5ecbe"
+LINK = "https://newsapi.org/v2/everything?q="
+def get_headlines(query):
+    query = '+'.join(query.split('_'))
+    with urllib.request.urlopen(f"{LINK}{query}&language=en&sortBy=popularity&apiKey={API_KEY}") as url:
+        data = json.loads(url.read().decode())
+        headlines = {"positive":[], "negative":[]}
+        for dic in data['articles'][:20]:
+            result = analyze_headline(dic['title'])
+            headlines[result].append({'url':dic['url'], 'title':dic['title']})
+        ret = json.dumps(headlines)
+        return JsonResponse(json.loads(ret), safe=False)
+
+
+"""threshold = 0
 def analyze_headline(headline):
  polarity = TextBlob(headline).sentiment.polarity
  if polarity >= threshold:
@@ -19,20 +42,27 @@ def analyze_headline(headline):
 API_KEY  = "318d913237d64b00b18eefa946b5ecbe"
 LINK = "https://newsapi.org/v2/everything?q="
 def get_headlines(query):
- query = '+'.join(query.split())
- with urllib.request.urlopen(f"{LINK}{query}&language=en&sortBy=popularity&apiKey={API_KEY}") as url:
-     data = json.loads(url.read().decode())
-     #print(*[dic['title'] for dic in data['articles'][:20]], sep='\n')
-     headlines = [{'url':dic['url'], 'title':dic['title'],
+    query = '+'.join(query.split())
+    with urllib.request.urlopen(f"{LINK}{query}&language=en&sortBy=popularity&apiKey={API_KEY}") as url:
+        data = json.loads(url.read().decode())
+        #print(*[dic['title'] for dic in data['articles'][:20]], sep='\n')
+        headlines = [{'url':dic['url'], 'title':dic['title'],
                    'label':analyze_headline(dic['title'])}
                   for dic in data['articles'][:20]]
-     ret = json.dumps(headlines)
-     return JsonResponse(json.loads(ret), safe=False)
+        ret = json.dumps(headlines)
+        return JsonResponse(json.loads(ret), safe=False)
+
+        headlines = {"positive":[], "negative":[]}
+        for dic in data['articles'][:20]:
+          result = analyze_headline(dic['title'])
+          headlines[result].append({'url':dic['url'], 'title':dic['title']})
+        ret = json.dumps(headlines)
+        return JsonResponse(json.loads(ret), safe=False)
 
 #print(*get_headlines("india pakistan tension"), sep='\n')
 
 #analyzed_headlines = scrapper(topic)
-#print(*analyzed_headlines, sep='\n')
+#print(*analyzed_headlines, sep='\n')"""
 
 """def test(temp):
     return get_headlines('new zealand terror attack')
@@ -43,5 +73,5 @@ def get_headlines(query):
 
 def home(request):
     #test('rahul')
-    return get_headlines('new zealand terror attack')
+    return get_headlines('new_zealand_terror_attack')
     #return render(request, 'index.html')
