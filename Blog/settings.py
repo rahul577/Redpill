03b295c29883748e0915dc57f1ccbe12808c460c
rahul577 <rahulbafila7@gmail.com>
2019-04-03 13:20:08 +0530

    s

--- Blog/settings.py
+++ Blog/settings.py
@@ -56,10 +56,14 @@ MIDDLEWARE = [
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 TEST_RUNNER = 'testing.DatabaselessTestRunner'
-
 
 ROOT_URLCONF = 'Blog.urls'
 
