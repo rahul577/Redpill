63cfc61f3bd8866a9f24ae92b91b66dfcd8766ed
rahul577 <rahulbafila7@gmail.com>
2019-04-03 12:09:55 +0530

    remove database

--- Blog/settings.py
+++ Blog/settings.py
@@ -56,12 +56,10 @@ MIDDLEWARE = [
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+TEST_RUNNER = 'testing.DatabaselessTestRunner'
+
 
 ROOT_URLCONF = 'Blog.urls'
 
